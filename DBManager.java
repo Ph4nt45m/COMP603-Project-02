3100c1eeef3ec0087bf0c260b3fe5b5919e2b039
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-06-08 12:56:55 +1200

    Update DBManager.java
    updated for adding to bookinglist and flag bookings

--- DBManager.java
+++ DBManager.java
@@ -18,14 +18,22 @@ public final class DBManager {
     private final String PASSWORD = "MAH";
     private final String projectFolderPath;
     private final String databaseFolderPath;
+    private ArrayList<Booking> bookingsList;
+    private boolean bookingSuccessful;
+    private LocalDate currentDate;
 
     Connection conn;
 
     public DBManager() {
         this.projectFolderPath = System.getProperty("user.dir");
         this.databaseFolderPath = "jdbc:derby:" + this.projectFolderPath + "/MarlAvenueHotelDB;create = true";
+        this.bookingsList = new ArrayList<>();
+        this.bookingSuccessful = false;
+        this.currentDate = LocalDate.now();
 
         establishConnection();
+//        readBookingsList();
+        flagBookings();
     }
 
     public Connection getConnection() {
@@ -41,6 +49,7 @@ public final class DBManager {
                 }
             } catch (SQLException ex) {
                 System.out.println("Connection failed. Error: " + ex.getMessage());
+                System.exit(0);
             }
         }
     }
@@ -55,8 +64,116 @@ public final class DBManager {
             }
         }
     }
-    
-    public void addToBookingList() {
-        
+
+    private void readBookingsList() {
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet booking = statement.executeQuery("SELECT * FROM BOOKINGLIST");
+
+            while (booking.next()) {
+                String[] bookingDate = booking.getString("BOOKINGDATE").split("/");
+                int dayBook = Integer.parseInt(bookingDate[0]);
+                int monthBook = Integer.parseInt(bookingDate[1]);
+                int yearBook = Integer.parseInt(bookingDate[2]);
+                Date dateBook = new Date(dayBook, monthBook, yearBook);
+                
+                String[] bookingLeave = booking.getString("BOOKINGDLEAVE").split("/");
+                int dayLeave = Integer.parseInt(bookingLeave[0]);
+                int monthLeave = Integer.parseInt(bookingLeave[1]);
+                int yearLeave = Integer.parseInt(bookingLeave[2]);
+                Date dateLeave = new Date(dayLeave, monthLeave, yearLeave);
+                
+                String firstName = booking.getString("FIRSTNAME");
+                String surname = booking.getString("SURNAME");
+                String roomType = booking.getString("ROOMTYPE");
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void addToBookingList(Booking booking) {
+        try {
+            String query = "INSERT INTO BOOKINGLIST (BOOKINGDATE, BOOKINGLEAVE, FIRSTNAME, SURNAME, ROOMTYPE, STUDENTDISCOUNT, "
+                    + "CHILDDISCOUNT, COST, PHONENUMBER, EMAIL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            try (PreparedStatement statement = conn.prepareStatement(query)) {
+                statement.setString(1, booking.dateBooked.toString());
+                statement.setString(2, booking.dateLeave.toString());
+                statement.setString(3, booking.firstName);
+                statement.setString(4, booking.surname);
+                statement.setString(5, booking.roomType.getRoomType());
+                statement.setBoolean(6, booking.roomType.isStudent());
+                statement.setBoolean(7, booking.roomType.hasChildren());
+                statement.setDouble(8, booking.roomType.calculateCost());
+                statement.setString(9, booking.phoneNumber);
+                statement.setString(10, booking.email);
+
+                statement.executeUpdate();
+                this.bookingSuccessful = true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public boolean isBSuccessful() {
+        return this.bookingSuccessful;
+    }
+
+    public void resetBSuccessful() {
+        this.bookingSuccessful = false;
+    }
+
+    private void flagBookings() {
+        try {
+            ResultSet bookings;
+
+            try (Statement statement = conn.createStatement()) {
+                bookings = statement.executeQuery("SELECT * FROM BOOKINGLIST");
+
+                while (bookings.next()) {
+                    String[] bookingDate = bookings.getString("BOOKINGDATE").split("/");
+                    int day = Integer.parseInt(bookingDate[0]);
+                    int month = Integer.parseInt(bookingDate[1]);
+                    int year = Integer.parseInt(bookingDate[2]);
+
+                    if (year == currentDate.getYear()) {
+                        if (month == currentDate.getMonthValue()) {
+                            if (day >= currentDate.getDayOfMonth()) {
+                                addFlaggedBooking(bookings);
+                            }
+                        }
+                    }
+                }
+            }
+
+            bookings.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void addFlaggedBooking(ResultSet booking) {
+        try {
+            String query = "INSERT INTO FLAGGEDBOOKINGS (BOOKINGDATE, BOOKINGLEAVE, FIRSTNAME, SURNAME, ROOMTYPE, STUDENTDISCOUNT, "
+                    + "CHILDDISCOUNT, COST, PHONENUMBER, EMAIL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            try (PreparedStatement statement = conn.prepareStatement(query)) {
+                statement.setString(1, booking.getString("BOOKINGDATE"));
+                statement.setString(2, booking.getString("BOOKINGLEAVE"));
+                statement.setString(3, booking.getString("FIRSTNAME"));
+                statement.setString(4, booking.getString("SURNAME"));
+                statement.setString(5, booking.getString("ROOMTYPE"));
+                statement.setBoolean(6, booking.getBoolean("STUDENTDISCOUNT"));
+                statement.setBoolean(7, booking.getBoolean("CHILDDISCOUNT"));
+                statement.setDouble(8, booking.getDouble("COST"));
+                statement.setString(9, booking.getString("PHONENUMBER"));
+                statement.setString(10, booking.getString("EMAIL"));
+
+                statement.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
