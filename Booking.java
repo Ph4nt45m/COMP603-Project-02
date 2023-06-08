27097dce0dba4d4d8f0355617b96e557548a758a
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-06-08 14:24:24 +1200

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -9,23 +9,16 @@ package Project_02;
  * @author snipi
  */
 public class Booking {
-    protected Room roomType;
     protected Date dateBooked;
     protected Date dateLeave;
     protected String firstName;
     protected String surname;
+    protected Room roomType;
     protected String phoneNumber;
     protected String email;
     private String addBookingSQLValue;
     
     public Booking() {
         this.addBookingSQLValue = "";
-    }
-    
-    public void addBookingSQLValue() {
-        this.addBookingSQLValue += "'" + dateBooked.toString() + "', '" + dateLeave.toString() + "', '"
-                + firstName + "', '" + surname + "', '" + roomType.getRoomType() + "', '" + roomType.isStudent()
-                + "', '" + roomType.hasChildren() + "', '" + roomType.calculateCost() + "', '" + phoneNumber
-                + "', '" + email + "'";
     }
 }
